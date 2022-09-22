c4e8c50603cfed3936ec06e298f9b500cbb41026
axelcastelnovo <renzo.axel.castelnovo@gmail.com>
2022-09-22 11:35:28 -0300

    button DragonFalseActive

---
+++ Assets/Proxi/DragonFalse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFalse : MonoBehaviour
+{
+    [SerializeField] private GameObject dragon;
+    public void DragonApagado()
+    {
+        dragon.SetActive(!dragon.activeSelf);
+    }
+}

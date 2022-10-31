d73e0a9b19b482933fe455232167ecf4fd9f5fcc
Renzo Axel Castelnovo <81384859+axelcastelnovo@users.noreply.github.com>
2022-10-31 17:00:51 -0300

    market

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject market;
+    [SerializeField]
+    private GameObject buttonMarket;
+    
+
+    public void MarketOpen()
+    {  
+        Time.timeScale = 0;
+        market.SetActive(true);
+        buttonMarket.SetActive(false);             
+    }
+
+    public void MarketClose()
+    {
+        Time.timeScale = 1;
+        market.SetActive(false);
+        buttonMarket.SetActive(true);
+    }
+}

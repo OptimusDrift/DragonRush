a4ee6a5df712b984c4da41232fa01820fd50f174
Drift (Brandon Urigo) <brandon.urigo@unraf.edu.ar>
2022-11-04 10:50:49 -0300

    ➕ADD: Fire Animation

---
+++ Assets/Scripts/Enemy/FireExplotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExplotion : MonoBehaviour
+{
+    public ParticleSystem explotion;
+    public bool play = false;
+    private int count = 0;
+    private void Update()
+    {
+        if (play && count == 0)
+        {
+            count++;
+            GetComponent<ParticleSystem>().Play();
+            explotion.Play();
+            play = false;
+        }
+        
+    }
+}
